7788f0d69f7c6cc07d003fb10c04faeadd1c97bc
DemonSlayer14 <61657351+DemonSlayer14@users.noreply.github.com>
2021-09-27 23:39:12 +0530

    Add files via upload

--- prac/app/solar-allocation-list/solar-allocation-list.service.ts
+++ prac/app/solar-allocation-list/solar-allocation-list.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { SolarHeater } from '../shared/SolarHeater';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolarAllocationListService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllocations() : Observable<number[]>{
-    return this.http.get('http://localhost:3050/getSolarHeaterIds') as Observable<number[]>;
+  getAllocations(): Observable<number[]> {
+    return this.http.get<number[]>('http://localhost:3050/getSolarHeaterIds').pipe(catchError(this.errHandler));
   }
 
-  getSolarHeaterbyId(id) : Observable<SolarHeater>{
-    console.log('id:',id);
-    return this.http.get(`http://localhost:3050/getSolarHeaterIds/${id}`) as Observable<SolarHeater>;
+  getSolarHeaterbyId(id): Observable<SolarHeater> {
+    return this.http.get<SolarHeater>(`http://localhost:3050/getSolarHeaterIds/${id}`).pipe(catchError(this.errHandler));
+  }
+
+  errHandler(err: HttpErrorResponse) {
+    return throwError(err.error.message);
   }
 }
